ae346c894dcd01a54f94eaefbe323014a68f1f1f
sherif Abdulrahman <sherifabdulrahman44@gmail.com>
2025-07-30 22:32:55 +0300

    first push on main branch.

--- app/src/main/java/com/example/ci_cd_implementation/MainActivity.kt
+++ app/src/main/java/com/example/ci_cd_implementation/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CICD_ImplementationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "CI/CD First Push",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
